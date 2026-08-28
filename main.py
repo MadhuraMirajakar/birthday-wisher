f6145e9e72046b8818d926b00ca14a8641fa8a7a
MadhuraMirajakar <madhuramirajakar@gmail.com>
2026-08-28 22:04:50 +0530

    upadated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import os
 import pandas
 import smtplib
+from email.message import EmailMessage
 
 MY_EMAIL = os.environ["MY_EMAIL"]
 MY_PASSWORD = os.environ["MY_PASSWORD"]
@@ -10,6 +11,7 @@ today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
+
 birthdays_dict = {
     (data_row["month"], data_row["day"]): data_row
     for (index, data_row) in data.iterrows()
@@ -19,18 +21,24 @@ if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
 
     file_path = "letter_3.txt"
+
     with open(file_path, encoding="utf-8") as letter_file:
         contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
+    contents = contents.replace("[NAME]", birthday_person["name"])
+
+    # Create UTF-8 email
+    email = EmailMessage()
+    email["Subject"] = "Happy Birthday!"
+    email["From"] = MY_EMAIL
+    email["To"] = birthday_person["email"]
+    email.set_content(contents, charset="utf-8")
+
+    # Send email through Gmail
     with smtplib.SMTP("smtp.gmail.com", port=587, timeout=30) as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject: Happy Birthday!\n\n{contents}"
-        )
+        connection.send_message(email)
 
     print(f"Birthday wish sent to {birthday_person['name']}.")
 else:
